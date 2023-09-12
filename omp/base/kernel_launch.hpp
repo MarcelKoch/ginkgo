0237be03c7ed6378e3fdd796e5eaaa09a994a4dc
Marcel Koch <marcel.koch@kit.edu>
2023-09-12 15:32:42 +0200

    tmp

--- omp/base/kernel_launch.hpp
+++ omp/base/kernel_launch.hpp
@@ -117,8 +117,7 @@ void run_kernel_impl(std::shared_ptr<const OmpExecutor> exec, KernelFunction fn,
     constexpr int block_size = 8;
     using remainders = syn::as_list<syn::range<0, block_size, 1>>;
 
-    if (cols <= 0) {
-        return;
+    if (cols <= 0) {        return;
     }
     select_run_kernel_sized(
         remainders(),
