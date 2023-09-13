2a15bf62f9a804fb7dc2047f55e795374deb2121
Marcel Koch <marcel.koch@kit.edu>
2023-09-13 11:43:06 +0200

    wip

--- omp/base/kernel_launch_solver.hpp
+++ omp/base/kernel_launch_solver.hpp
@@ -60,8 +60,7 @@ void run_kernel_solver(std::shared_ptr<const OmpExecutor> exec,
 {
     run_kernel_impl(
         exec,
-        fn,
-        size,
+        fn,        size,
         map_to_device_solver(args, static_cast<int64>(default_stride))...);
 }
 
